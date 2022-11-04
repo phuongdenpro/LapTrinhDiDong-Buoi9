c189c1fd66647cab8eb3826c0663ed38cfd2c529
phuong <phuongdenpro@gmail.com>
2022-11-04 10:03:37 +0700

    finish bai 1

--- Bai1/App.js
+++ Bai1/App.js
@@ -1,79 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { useRef, useState  } from 'react';
-import { Animated, StyleSheet, Text, View } from 'react-native';
-import { Button, Image } from 'react-native';
-import icon from './assets/donut_red.png'
-
+import { StatusBar } from "expo-status-bar";
+import { useRef, useState } from "react";
+import { Animated, StyleSheet, Text, View } from "react-native";
+import { Button, Image } from "react-native";
+import icon from "./assets/download.jpg";
 
 export default function App() {
+  const [location, setLocation] = useState({
+    x: null,
+    y: null,
+    marginLeft: new Animated.Value(10),
+    marginTop: new Animated.Value(10),
+  });
 
-  const [location, setLocation] 
-      = useState({
-        x: null,
-        y: null,
-        marginLeft: new Animated.Value(10),
-        marginTop: new Animated.Value(10)
-
-      });
-
-  function onPress(evt){
-       
-    
-    console.log('====================================');
+  function onPress(evt) {
+    console.log("====================================");
     var x = evt.nativeEvent.locationX;
-    console.log('====================================');
+    console.log("====================================");
     console.log(x);
-    console.log('====================================');
+    console.log("====================================");
     var y = evt.nativeEvent.locationY;
-    console.log('====================================');
+    console.log("====================================");
     setLocation({
       x: x,
       y: y,
       marginLeft: x,
-      marginTop: y
-
-    })
-    
-    
+      marginTop: y,
+    });
   }
-  function onMove(evt){
-    
-    
-    
-    console.log('====================================');
+  function onMove(evt) {
+    console.log("====================================");
     console.log(location);
-    console.log('====================================');
+    console.log("====================================");
     //setLocation({marginLeft: x, marginTop: y })
   }
-  function onRelease(){
+  function onRelease() {
     console.log("Realse!");
   }
-  const {marginTop, marginLeft} = location;
+  const { marginTop, marginLeft } = location;
   return (
     <View
-      onStartShouldSetResponder={()=>true}
-      onMoveShouldSetResponder= {()=>true}
+      onStartShouldSetResponder={() => true}
+      onMoveShouldSetResponder={() => true}
       onResponderGrant={onPress}
       onResponderMove={onMove}
-      onResponderRelease = {onRelease}
-      style={styles.container}>
+      onResponderRelease={onRelease}
+      style={styles.container}
+    >
+      <Text style={{color:'green', fontSize:15}}>Touch where you need to go!</Text>
 
-      <Text>Show something!</Text>
-     
       <Animated.Image
-       source = {icon}
-       style={{marginLeft: marginLeft, marginTop: marginTop}}
-       >
-
-      </Animated.Image>
-
+        source={icon}
+        style={{ marginLeft: marginLeft, marginTop: marginTop }}
+      ></Animated.Image>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
-
+    flex: 1,
   },
 });
